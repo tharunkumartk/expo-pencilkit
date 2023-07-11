7a5d69e39e1565080dd46f694578e51d60c60892
TharunTK <tharuntk@princeton.edu>
2023-07-12 00:36:42 +0300

    updated view

--- src/ExpoPencilkitView.tsx
+++ src/ExpoPencilkitView.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { ViewProps } from "react-native";
 
 export type Props = {
-  fileName: string;
+  sampleId: string;
 } & ViewProps;
 
 const NativeView: React.ComponentType<Props> =
